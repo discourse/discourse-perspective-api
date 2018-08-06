49d4ec567dfb3a4f1bd1ffe8fc5f2478444af665
Erick Guan <fantasticfears@gmail.com>
2018-08-07 00:25:14 +0200

    Add requset spec

---
+++ spec/requests/post_toxicity_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe ::Etiquette::PostToxicityController do
+  let(:api_endpoint) { "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=" }
+  let(:api_response_toxicity_body) { '{"attributeScores": {"TOXICITY": {"spanScores": [{"begin": 0,"end": 11,"score": {"value": 0.015122943,"type": "PROBABILITY"}}],"summaryScore": {"value": 0.015122943,"type": "PROBABILITY"}}},"languages": ["en"]}' }
+  let(:api_response_high_toxicity_body) { '{"attributeScores": {"TOXICITY": {"spanScores": [{"begin": 0,"end": 11,"score": {"value": 0.915122943,"type": "PROBABILITY"}}],"summaryScore": {"value": 0.915122943,"type": "PROBABILITY"}}},"languages": ["en"]}' }
+  let(:headers) { {"ACCEPT" => "applicaiton/json", "HTTP_ACCEPT" => "application/json"} }
+  let(:post) { Fabricate(:post, user: log_in) }
+
+  describe '.show' do
+    it 'returns the score if above threshold' do
+      stub_request(:post, api_endpoint).to_return(status: 200, body: api_response_high_toxicity_body, headers: {})
+      get '/etiquette/post_toxicity', params: { concat: 'Fuck. This is outrageous and dumb. Go to hell.' }, headers: headers
+      json = JSON.parse(response.body)
+      expect(json['score']).to eq 0.915122943
+    end
+
+    it 'returns nothing if under threshold' do
+      stub_request(:post, api_endpoint).to_return(status: 200, body: api_response_toxicity_body, headers: {})
+      get '/etiquette/post_toxicity', params: { concat: 'Fuck. This is outrageous and dumb. Go to hell.' }, headers: headers
+      json = JSON.parse(response.body)
+      expect(json).to eq({})
+    end
+
+    it 'returns nothing if any network errors' do
+      stub_request(:post, api_endpoint).to_return(status: 403)
+      get '/etiquette/post_toxicity', params: { concat: 'Fuck. This is outrageous and dumb. Go to hell.' }, headers: headers
+      json = JSON.parse(response.body)
+      expect(json).to eq({})
+    end
+  end
+end
