0165a692a227b50378decf3a4777208b264f88bf
Loïc Guitaut <loic@discourse.org>
2024-05-28 11:17:48 +0200

    DEV: Update rubocop-discourse to version 3.8.0

--- jobs/flag_toxic_post.rb
+++ jobs/flag_toxic_post.rb
@@ -3,7 +3,7 @@
 module Jobs
   class FlagToxicPost < ::Jobs::Base
     def execute(args)
-      raise Discourse::InvalidParameters.new(:post_id) unless args[:post_id].present?
+      raise Discourse::InvalidParameters.new(:post_id) if args[:post_id].blank?
 
       unless SiteSetting.perspective_enabled? &&
                SiteSetting.perspective_flag_post_min_toxicity_enable?
@@ -11,7 +11,7 @@ module Jobs
       end
 
       post = Post.where(id: args[:post_id]).first
-      return unless post.present? && !post.trashed?
+      return if post.blank? || post.trashed?
 
       DiscoursePerspective.check_post_toxicity(post)
     end
